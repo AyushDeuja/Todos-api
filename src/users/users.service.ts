33013037220075abf2afbb8129df8371064ea013
AyushDeuja <ayushdeuja11@gmail.com>
2025-03-21 18:08:43 +0545

    crud operation on users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaClient } from '@prisma/client';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaClient) {}
+  async create(createUserDto: CreateUserDto) {
+    //To manage uniqueness of email and mobile
+    let user = await this.prisma.user.findUnique({
+      where: { email: createUserDto.email },
+    });
+    if (user) {
+      throw new BadRequestException('This email has already been taken');
+    }
+    user = await this.prisma.user.findUnique({
+      where: { mobile: createUserDto.mobile },
+    });
+    if (user) {
+      throw new BadRequestException(`This mobile no. has already been taken`);
+    }
+
+    //To hash the password created
+    createUserDto.password = await hash(createUserDto.password, 10);
+
+    //Create user
+    return this.prisma.user.create({
+      data: createUserDto,
+    });
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.findOne(id);
+
+    if (updateUserDto.password) {
+      updateUserDto.password = await hash(updateUserDto.password, 10);
+    }
+
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    await this.findOne(id);
+    return this.prisma.user.delete({
+      where: { id },
+    });
   }
 }
