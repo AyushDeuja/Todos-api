3d2dceab3b314cfa7110e426f059284c2280e69e
AyushDeuja <ayushdeuja11@gmail.com>
2025-04-09 14:06:45 +0545

    profile update created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { Public } from 'src/helpers/public';
+import { Payload } from 'src/interfaces/payload';
+import { UpdateProfileDto } from './dto/update-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -17,5 +19,18 @@ export class AuthController {
   @Post('login')
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
+  }
+
+  @Get('profile')
+  profile(@Req() req: Payload) {
+    return this.authService.profile(req.payload.id);
+  }
+
+  @Patch('profile')
+  updateProfile(
+    @Req() req: Payload,
+    @Body() updateProfileDto: UpdateProfileDto,
+  ) {
+    return this.authService.updateProfile(req.payload.id, updateProfileDto);
   }
 }
